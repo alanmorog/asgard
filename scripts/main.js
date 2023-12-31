b2d19d6d21a4bf26555adf14b7114248f5622156
Alanmoro <alan.gabriel.moro@gmail.com>
2023-12-30 23:03:28 -0300

    Responsive Update

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const tabEl = document.querySelector('button[data-bs-toggle="tab"]')
+tabEl.addEventListener('shown.bs.tab', event => {
+  event.target // newly activated tab
+  event.relatedTarget // previous active tab
+})
