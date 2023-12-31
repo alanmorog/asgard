c21486c84f522609d00e216a853589f1fce7f343
Alanmoro <alan.gabriel.moro@gmail.com>
2023-12-31 05:20:33 -0300

    lik

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,4 @@
-/* const tabEl = document.querySelector('button[data-bs-toggle="tab"]')
-tabEl.addEventListener('shown.bs.tab', event => {
-  event.target // newly activated tab
-  event.relatedTarget // previous active tab
-}) */
+
 
 //mercadopago---------------------------
 
